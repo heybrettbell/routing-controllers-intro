2a7badda6800be6043cfa322bc0cfb25ea2d061a
heybrettbell <brett@grassrootsonline.ca>
2017-07-19 14:50:38 -0400

    Added kitten page and dynamic images

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -15,7 +15,11 @@ class PagesController < ApplicationController
   def contest
     @header = "This is the contest page"
     render :contest
+  end
 
+  def kitten
+  requested_size = params[:size]
+  @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
   end
 
 
