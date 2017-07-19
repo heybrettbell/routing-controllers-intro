326ec575d641e7c6107492c391bc274812cb7734
heybrettbell <brett@grassrootsonline.ca>
2017-07-19 14:36:55 -0400

    Added @header functionality

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,21 @@
 class PagesController < ApplicationController
 
   def welcome
+    @header = "This is the welcome page"
     render :welcome
+
   end
 
   def about
+    @header = "This is the about page"
     render :about
+
+  end
+
+  def contest
+    @header = "This is the contest page"
+    render :contest
+
   end
 
 
